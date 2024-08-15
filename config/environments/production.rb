4daa6d0bbb9f7235678650092c74732c0c3e476c
Fred Willmore <fredwillmore@gmail.com>
2024-08-15 11:37:48 -0700

    removed database dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,9 +36,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -69,7 +66,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "fredwillmore_production"
+  # config.active_job.queue_name_prefix = "fredwillmore_site_production"
 
   config.action_mailer.perform_caching = false
 
@@ -83,9 +80,6 @@ Rails.application.configure do
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
